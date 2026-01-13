39aa1e448c12578cb96ffa25273041d72ab66d04
SumbizAVGNT <kirillkorolev2003a@gmail.com>
2026-01-13 12:49:21 +0300

    первый коммит

---
+++ src/main/java/com/moonrein/moonEnchant/MoonEnchant.java
@@ -0,0 +1,17 @@
+package com.moonrein.moonEnchant;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MoonEnchant extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
